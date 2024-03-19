e1025686f97db11b39e89bc1562b873316a809dd
RogerKimJazzLover <minseungkim1017@gmail.com>
2024-03-19 13:42:55 +0900

    Considering using user-agent

--- browser.py
+++ browser.py
@@ -1,16 +1,20 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-import os
+
 import time
+
 
 class Browser:
     def __init__(self):
+        # chrome_options = Options()
+        # chrome_options.add_argument("--user-agent=Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36")
+        # self.driver = webdriver.Chrome(options=chrome_options)
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(3)
-        self.waitTime = 1 # wait 1 second for loading
+        self.waitTime = 0.5 # wait 1 second for loading
         self.urlList = []
 
     def goToPage(self,url):
@@ -19,16 +23,16 @@ class Browser:
     def getPageSource(self):
         return self.driver.page_source
 
-    def expandComments(self):
-        try:
-            # loading all comments.
-            # if all comments is loaded, exception will raise on click function
-            while(True):
-                expandScript = "return (a = document.getElementsByClassName('Z4IfV')[0].click())"
-                self.driver.execute_script(expandScript)
-                time.sleep(0.1)
-        except:
-            pass
+    # def expandComments(self):
+    #     try:
+    #         # loading all comments.
+    #         # if all comments is loaded, exception will raise on click function
+    #         while(True):
+    #             expandScript = "return (a = document.getElementsByClassName('Z4IfV')[0].click())"
+    #             self.driver.execute_script(expandScript)
+    #             time.sleep(0.1)
+    #     except:
+    #         pass
     
     def getPageSourceCond(self, element):
         delay = 30
@@ -65,12 +69,12 @@ class Browser:
             if dup > 2:
                 break
         
-    def collectDpageUrl(self, data):
-        r = data.split(f'href="/{self.username}/reel/')[1:]
-        for i in r:
-            dPageLink = "https://www.instagram.com/reel/"+i.split('"')[0]+"?hl=en"
-            if dPageLink not in self.urlList:
-                self.urlList.append(dPageLink)
+    # def collectDpageUrl(self, data):
+    #     r = data.split(f'href="/{self.username}/reel/')[1:]
+    #     for i in r:
+    #         dPageLink = "https://www.instagram.com/reel/"+i.split('"')[0]+"?hl=en"
+    #         if dPageLink not in self.urlList:
+    #             self.urlList.append(dPageLink)
             
     def __del__(self):
         try:
