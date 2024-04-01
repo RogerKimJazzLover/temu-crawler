4fc679759499c44363b7f6c012c14abb15a3a478
RogerKimJazzLover <minseungkim1017@gmail.com>
2024-04-01 16:02:42 +0900

    Save to csv

--- crawl_shein.py
+++ crawl_shein.py
@@ -1,8 +1,12 @@
-import bs4
 import time
-from browser import Browser
 from collections import Counter
+
+import bs4
+from browser import Browser
 from bs4 import BeautifulSoup as bs
+
+import pandas as pd
+from tabulate import tabulate
 
 def get_repeated_classes(soup) -> dict:
     elements_with_class = soup[0].find_all(class_=True)
@@ -39,6 +43,14 @@ def get_element_with_repeated_clases(repeated_classes: dict, filtered_data: bs4.
     '''
     return elements
 
+def extract_text(code: str) -> str:
+    '''
+    Takes a raw html code, with one string
+    Then return the natural language part
+    '''
+    # code = bs(code, "html.parser")
+    return code.get_text()
+
 def main():
     brsr = Browser()    
     brsr.goToPage("https://asia.shein.com/campaigns/mostpopularitems")
@@ -48,17 +60,28 @@ def main():
     brsr.scrollPageToBottom()
     page = brsr.getPageSource() 
     soup = bs(page, "html.parser")
+    brsr.driver.quit()
 
+    # FILTERING STEP 1. "Getting the divs"
     filtered_data = soup.find_all("div", attrs={"class":"product-list"})
     repeated_classes = get_repeated_classes(soup=filtered_data)
     elements = get_element_with_repeated_clases(repeated_classes, filtered_data)
-
-    # filtering only the ones I need
     # For mobile: desired_keys = ['price', 'title', 'rank-product-item__r', 'item-r__t', 'rank-product-item']
     desired_keys = ["S-product-item__price", "S-product-item__name"]
     elements = {key: elements[key] for key in desired_keys}
+    
+    # FILTERING STEP 2. "Getting the text"
+    for key, value in elements.items():
+        arr = []
+        for i in range(len(value)):
+            arr.append(extract_text(value[i]))
+        elements[key] = arr
 
-    brsr.driver.quit()
+    #Defining data for csv
+    elements = pd.DataFrame(elements)
+    elements.rename(columns = {'S-product-item__price' : '가격', 'S-product-item__name' : '상품명'}, inplace = True)
+    elements.to_csv("./data/shein_most_popular_item.csv", encoding="euc-kr", index=False)
+    # print(tabulate(elements, headers='keys', tablefmt='psql'))
 
 if __name__ == "__main__":
     main()
